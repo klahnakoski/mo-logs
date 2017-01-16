c5ead38fd7f86f68db524b36145e8cfd8abc14d3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-01-16 16:45:39 -0500

    advance version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='MoDots',
-    version="1.0.17001",
+    version="1.0.17017",
     description='More Logs!',
     long_description=long_desc,
     author='Kyle Lahnakoski',
