f84d58b4546e9595bda7e8b145de0d14a7f3dbe8
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-02-02 22:09:42 -0500

    update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-logs',
-    version="1.0.17029",
+    version="1.0.17035",
     description='More Logs! Structured Logging and Exception Handling',
     long_description=long_desc,
     author='Kyle Lahnakoski',
