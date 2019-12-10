b5e7d0752fdd5f97ae01bc439a23499a1fdc9747
Kyle Lahnakoski <kyle@lahnakoski.com>
2019-12-10 17:18:13 -0500

    try this

--- vendor/mo_dots/__init__.py
+++ vendor/mo_dots/__init__.py
@@ -445,7 +445,7 @@ def _set_attr(obj_, path, value):
 
     try:
         setattr(obj, attr_name, new_value)
-        _ = ""+getattr(obj, attr_name)
+        print(""+getattr(obj, attr_name))
         return old_value
     except Exception as e:
         try:
