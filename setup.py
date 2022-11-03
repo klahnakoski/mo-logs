d19559aba8db49c97766a171d4367db8859c9e47
Kyle Lahnakoski <kyle@lahnakoski.com>
2022-11-03 08:17:50 -0400

    update version number

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     name='mo-logs',
     packages=["mo_logs"],
     url='https://github.com/klahnakoski/mo-logs',
-    version='7.215.22307',
+    version='7.216.22307',
     zip_safe=False
 )
