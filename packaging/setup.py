11c30a9bb06abd90a373d6355bca20ed4c64841a
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-07-25 21:47:28 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -15,6 +15,6 @@ setup(
     name='mo-logs',
     packages=["mo_logs"],
     url='https://github.com/klahnakoski/mo-logs',
-    version='8.417.23168',
+    version='8.425.23207',
     zip_safe=False
 )
