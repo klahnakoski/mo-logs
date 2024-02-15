5a8cb15b1884b95c7243700f95d1508b0682a30a
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-02-14 21:11:28 -0500

    use str

--- mo_logs/log_usingLogger.py
+++ mo_logs/log_usingLogger.py
@@ -44,7 +44,7 @@ class StructuredLogger_usingLogger(StructuredLogger):
             cause = exceptions.Except.wrap(cause)
             import sys
 
-            sys.stderr.write("can not write to logger: " + str(cause))
+            sys.stderr.write("can not write to logger: " + text(cause))
 
     def stop(self):
         try:
